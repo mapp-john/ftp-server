9ef960fafe075315c1d0e4ede4d215acfd07edb9
johnmapp <you@example.com>
2020-07-15 21:12:44 -0600

    Added Random user/pass, and Dir/Port as variables that can be passed into the object

--- ftp_server.py
+++ ftp_server.py
@@ -1,17 +1,47 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import ThreadedFTPServer
-import threading
+import threading,random
 
-class FtpServer:
 
-    authorizer = DummyAuthorizer()
-    authorizer.add_user('dummyuser', 'dummypass', '/tmp', perm='elradfmw')
 
-    handler = FTPHandler
-    handler.authorizer = authorizer
 
-    SRV = ThreadedFTPServer(('0.0.0.0',2121), handler)
+########################
+# Random Password Generator
+# Returns 14 character string
+#
+def Random_String(num=14):
+    Random_Str = ''
+    while len(Random_Str) < num:
+        char = 'abcdefghijklmnopqrstuvwxyz'
+        CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        NUM = '1234567890'
+
+        Random_Str += random.choice(char)
+        Random_Str += random.choice(NUM)
+        Random_Str += random.choice(CHAR)
+
+    return Random_Str
+
+########################
+# FTP Server Class
+# Dir = str()
+#   Ex: '/tmp'
+# Port = int()
+#   Ex: 2121
+class FtpServer(object):
+    # Create Random Username and Password
+    def __init__(self,Dir='/tmp',Port=2121):
+        self.Dir = Dir
+        self.Port = Port
+        self.user = Random_String()
+        self.Pass = Random_String()
+        self.authorizer = DummyAuthorizer()
+        self.authorizer.add_user(self.user, self.Pass, self.Dir, perm='elradfmw')
+        self.handler = FTPHandler
+        self.handler.authorizer = self.authorizer
+
+        self.SRV = ThreadedFTPServer(('0.0.0.0',self.Port), self.handler)
 
     def _run_server(self):
         self.SRV.serve_forever()
@@ -24,4 +54,17 @@ class FtpServer:
 
     def stop(self):
         self.SRV.close_all()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
